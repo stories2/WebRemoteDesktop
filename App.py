acc13b888d1ba1a1396fb5083172ce67b7338437
Kim HyunWoo <stories2@naver.com>
2019-01-18 11:41:12 +0900

    appSocket: cannot import app

--- App.py
+++ App.py
@@ -1,6 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+from . import create_app, socketio
+
+app = create_app()
+
+if __name__ == '__main__':
+    socketio.run(app)
 
 @app.route('/')
 def hello_world():
+    app.logger.info("hello world!")
     return 'Hi!'

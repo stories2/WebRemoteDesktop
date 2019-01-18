6fe581b69ad9a44cdd75881cd42b3d79fe1a990c
Kim HyunWoo <stories2@naver.com>
2019-01-18 13:12:39 +0900

    App: fastest way to solve socketio setup

--- Route/appSocket.py
+++ Route/appSocket.py
@@ -1,27 +1,6 @@
-from flask import Blueprint, current_app
-from App import socketio
+from flask import Blueprint
 appSocket = Blueprint('socket', __name__)
 
 @appSocket.route('/')
 def index():
     return "socket"
-
-@socketio.on('screen', namespace='/remote')
-def client_screen(screen) :
-    current_app.logger.info("screen data accepted from client")
-    return "ok"
-
-@socketio.on('feedback', namespace='/remote')
-def client_feedback(feedback):
-    current_app.logger.info("feedback data accepted from client")
-    return "ok"
-
-@socketio.on('connect', namespace='/remote')
-def client_connect():
-    current_app.logger.info("client connected")
-    return "ok"
-
-@socketio.on('disconnect', namespace='/remote')
-def client_disconnect():
-    current_app.logger.info("client disconnected")
-    return "ok"
--- __init__.py
+++ __init__.py
@@ -1,21 +1,9 @@
-from flask import Flask, render_template
-from logging.handlers import RotatingFileHandler
-from flask_socketio import SocketIO
+from flask import Flask
 from Route.appSocket import appSocket
-import logging, os
-
-socketio = SocketIO()
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(appSocket, url_prefix='/socket')
-
-    handler = RotatingFileHandler('default.log', maxBytes=10000, backupCount=1)
-    handler.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
-
     app.debug = True
-
-    socketio.init_app(app)
 
     return app
